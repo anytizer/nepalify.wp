1a36685c6326767c8c4a125a51cfe3a13b98814a
anytizer <anytizer@bimal.org.np>
2019-09-09 02:53:11 -0600

    formatting

--- trunk/nepalify.php
+++ trunk/nepalify.php
@@ -8,143 +8,141 @@
  * Author URI: http://bimal.org.np/
  */
 
-if(!defined("ABSPATH")){
-	exit;
+if (!defined("ABSPATH")) {
+    exit;
 }
 
 class nepalify
 {
-	public function create_categories()
-	{
-		$zones = array(
-			"Mechi" => array(
-				// https://en.wikipedia.org/wiki/Mechi_Zone
-				"Ilam",
-				"Jhapa",
-				"Panchthar",
-				"Taplejung",
-			),
-			"Koshi" => array(
-				// https://en.wikipedia.org/wiki/Koshi_Zone
-				"Bhojpur",
-				"Dhankuta",
-				"Morang",
-				"Sankhuwasabha",
-				"Sunsari",
-				"Terhathum",
-			),
-			"Sagarmatha" => array(
-				// https://en.wikipedia.org/wiki/Sagarmatha_Zone
-				"Khotang",
-				"Okhaldhunga",
-				"Solukhumbu",
-				"Udayapur",
-				"Saptari",
-				"Siraha",
-			),
-			"Janakpur" => array(
-				// https://en.wikipedia.org/wiki/Janakpur_Zone
-				"Dhanusa",
-				"Dolakha",
-				"Mahottari",
-				"Ramechhap",
-				"Sarlahi",
-				"Sindhuli",
-			),
-			"Bagmati" => array(
-				// https://en.wikipedia.org/wiki/Bagmati_Zone
-				"Bhaktapur",
-				"Dhading",
-				"Lalitpur",
-				"Kathmandu",
-				"Kavrepalanchok",
-				"Nuwakot",
-				"Rasuwa",
-				"Sindhupalchok",
-			),
-			"Narayani" => array(
-				// https://en.wikipedia.org/wiki/Narayani_Zone
-				"Bara",
-				"Parsa",
-				"Rautahat",
-				"Chitwan",
-				"Makwanpur",
-			),
-			"Gandaki" => array(
-				// https://en.wikipedia.org/wiki/Gandaki_Zone
-				"Gorkha",
-				"Kaski",
-				"Lamjung",
-				"Manang",
-				"Syangja",
-				"Tanahu",
-			),
-			"Lumbini" => array(
-				// https://en.wikipedia.org/wiki/Lumbini_Zone
-				"Arghakhanchi",
-				"Gulmi",
-				"Kapilvastu",
-				"Nawalparasi",
-				"Palpa",
-				"Rupandehi",
-			),
-			"Dhaulagiri" => array(
-				// https://en.wikipedia.org/wiki/Dhaulagiri_Zone
-				"Baglung",
-				"Mustang",
-				"Myagdi",
-				"Parbat",
-			),
-			"Rapti" => array(
-				// https://en.wikipedia.org/wiki/Rapti_Zone
-				"Dang",
-				"Pyuthan",
-				"Rolpa",
-				"Rukum",
-				"Salyan",
-			),
-			"Karnali" => array(
-				// https://en.wikipedia.org/wiki/Karnali_Zone
-				"Dolpa",
-				"Humla",
-				"Jumla",
-				"Kalikot",
-				"Mugu",
-			),
-			"Bheri" => array(
-				// https://en.wikipedia.org/wiki/Bheri_Zone
-				"Banke",
-				"Bardiya",
-				"Dailekh",
-				"Jajarkot",
-				"Surkhet",
-			),
-			"Seti" => array(
-				// https://en.wikipedia.org/wiki/Seti_Zone
-				"Achham",
-				"Bajhang",
-				"Bajura",
-				"Doti",
-				"Kailali",
-			),
-			"Mahakali" => array(
-				// https://en.wikipedia.org/wiki/Mahakali_Zone
-				"Baitadi",
-				"Dadeldhura",
-				"Darchula",
-				"Kanchanpur",
-			),
-		);
+    public function create_categories()
+    {
+        $zones = array(
+            "Mechi" => array(
+                // https://en.wikipedia.org/wiki/Mechi_Zone
+                "Ilam",
+                "Jhapa",
+                "Panchthar",
+                "Taplejung",
+            ),
+            "Koshi" => array(
+                // https://en.wikipedia.org/wiki/Koshi_Zone
+                "Bhojpur",
+                "Dhankuta",
+                "Morang",
+                "Sankhuwasabha",
+                "Sunsari",
+                "Terhathum",
+            ),
+            "Sagarmatha" => array(
+                // https://en.wikipedia.org/wiki/Sagarmatha_Zone
+                "Khotang",
+                "Okhaldhunga",
+                "Solukhumbu",
+                "Udayapur",
+                "Saptari",
+                "Siraha",
+            ),
+            "Janakpur" => array(
+                // https://en.wikipedia.org/wiki/Janakpur_Zone
+                "Dhanusa",
+                "Dolakha",
+                "Mahottari",
+                "Ramechhap",
+                "Sarlahi",
+                "Sindhuli",
+            ),
+            "Bagmati" => array(
+                // https://en.wikipedia.org/wiki/Bagmati_Zone
+                "Bhaktapur",
+                "Dhading",
+                "Lalitpur",
+                "Kathmandu",
+                "Kavrepalanchok",
+                "Nuwakot",
+                "Rasuwa",
+                "Sindhupalchok",
+            ),
+            "Narayani" => array(
+                // https://en.wikipedia.org/wiki/Narayani_Zone
+                "Bara",
+                "Parsa",
+                "Rautahat",
+                "Chitwan",
+                "Makwanpur",
+            ),
+            "Gandaki" => array(
+                // https://en.wikipedia.org/wiki/Gandaki_Zone
+                "Gorkha",
+                "Kaski",
+                "Lamjung",
+                "Manang",
+                "Syangja",
+                "Tanahu",
+            ),
+            "Lumbini" => array(
+                // https://en.wikipedia.org/wiki/Lumbini_Zone
+                "Arghakhanchi",
+                "Gulmi",
+                "Kapilvastu",
+                "Nawalparasi",
+                "Palpa",
+                "Rupandehi",
+            ),
+            "Dhaulagiri" => array(
+                // https://en.wikipedia.org/wiki/Dhaulagiri_Zone
+                "Baglung",
+                "Mustang",
+                "Myagdi",
+                "Parbat",
+            ),
+            "Rapti" => array(
+                // https://en.wikipedia.org/wiki/Rapti_Zone
+                "Dang",
+                "Pyuthan",
+                "Rolpa",
+                "Rukum",
+                "Salyan",
+            ),
+            "Karnali" => array(
+                // https://en.wikipedia.org/wiki/Karnali_Zone
+                "Dolpa",
+                "Humla",
+                "Jumla",
+                "Kalikot",
+                "Mugu",
+            ),
+            "Bheri" => array(
+                // https://en.wikipedia.org/wiki/Bheri_Zone
+                "Banke",
+                "Bardiya",
+                "Dailekh",
+                "Jajarkot",
+                "Surkhet",
+            ),
+            "Seti" => array(
+                // https://en.wikipedia.org/wiki/Seti_Zone
+                "Achham",
+                "Bajhang",
+                "Bajura",
+                "Doti",
+                "Kailali",
+            ),
+            "Mahakali" => array(
+                // https://en.wikipedia.org/wiki/Mahakali_Zone
+                "Baitadi",
+                "Dadeldhura",
+                "Darchula",
+                "Kanchanpur",
+            ),
+        );
 
-		foreach($zones as $zone => $districts)
-		{
-			$zone_category_id = wp_create_category($zone, 0);
-			foreach($districts as $district)
-			{
-				$district_category_id = wp_create_category($district, $zone_category_id);
-			}
-		}
-	}
+        foreach ($zones as $zone => $districts) {
+            $zone_category_id = wp_create_category($zone, 0);
+            foreach ($districts as $district) {
+                $district_category_id = wp_create_category($district, $zone_category_id);
+            }
+        }
+    }
 }
 
 $nepalify = new nepalify();
